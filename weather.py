34924dc45b4dfe77c6b88902e6b47b0cbe014174
sneha31415 <sneha.singh.31415@gmail.com>
2024-07-05 08:54:02 +0530

    added the get_current_weather() fucntion in weather.py which retrives the desired city's data using open weather's api

---
+++ weather.py
@@ -0,0 +1,32 @@
+#this is a python weather module that will retrive the weather for any city using the open weather api
+
+from dotenv import load_dotenv
+from pprint import pprint
+import requests
+import os
+
+#load the api key value from .env file 
+
+load_dotenv()
+
+# default value is mumbai 
+def get_current_weather(city = "Mumbai"):
+
+    request_url = f'http://api.openweathermap.org/data/2.5/weather?appid={os.getenv("API_KEY")}&q={city}&units=imperial'
+
+    weather_data = requests.get(request_url).json()
+
+    return weather_data
+
+# if this file is called directly
+
+if __name__ == "__main__":
+    # welcome to the app
+    print('\n*** Get Current Weather Conditions ***\n')
+
+    city = input("\nPlease enter a city name: ")
+
+    weather_data = get_current_weather(city)
+
+    print("\n")
+    pprint(weather_data)
